ddfba8b7dcaa02a4771a333551377b90f6936bc3
Sanket Kapse <sanket8397@gmail.com>
2024-03-11 15:02:10 -0700

    login route implemented

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -32,4 +32,8 @@ def login():
         if auth.username != email or auth.password != password:
             return "invalid credentials", 401
         else:
-            return createJWT(auth.username, os.environ.get("JWT_SECRET"))
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    else:
+        return "invalid credentials", 401
+
+
