067b0cdc847268a1eebb7b023ed826ae8e34d3d5
Sanket Kapse <sanket8397@gmail.com>
2024-03-11 15:21:38 -0700

    jwt token creation function implemented

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -36,4 +36,14 @@ def login():
     else:
         return "invalid credentials", 401
 
-
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
+        },
+        secret,
+        algorith="HS256",
+    )
